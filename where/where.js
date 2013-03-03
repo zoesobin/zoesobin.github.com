63de9fd1ff6dad835eb66c8accb8e1f4038a1e25
zoesobin <zoe.sobin@tufts.edu>
2013-03-03 15:17:31 -0500

    created stations

--- where/where.js
+++ where/where.js
@@ -1,46 +1,26 @@
 var stations =[
-{'name': 'ALEWIFE NB','lat': 42.395428,'lon': -71.142483, 'key': 'RALEN'},
-{'name': 'DAVIS NB' , 'lat':42.39674,'lon': -71.121815, 'key': 'RDAVN'}, 
-{'name': 'DAVIS SB' ,'lat':42.39674,'lon': -71.121815, 'key':'RDAVS'},
-{'name': 'PORTER NB', 'lat':42.3884,	'lon':-71.119149, 'key':'RPORN'},
-{'name': 'PORTER SB','lat': 42.3884,	'lon':-71.119149, 'key':'RPORS'},
-{'name': 'HARVARD NB','lat':42.373362, 'lon':-71.118956, 'key':'RHARN'},
-{'name': 'HARVARD SB','lat':42.373362, 'lon':-71.118956, 'key':'RHARS'},
-{'name': 'CENTRAL NB','lat':42.365486, 'lon':-71.103802, 'key':'RCENN'},
-{'name': 'CENTRAL SB','lat':42.365486, 'lon':-71.103802, 'key':'RCENS'},
-{'name': 'KENDALL NB','lat':42.36249079, 'lon':-71.08617653, 'key':'RKENN'},
-{'name': 'KENDALL SB','lat':42.36249079, 'lon':-71.08617653, 'key':'RKENS'},
-{'name': 'CHARLES MGH NB','lat':42.361166, 'lon':-71.070628, 'key':'RMGHN'},
-{'name': 'CHARLES MGH SB','lat':42.361166, 'lon':-71.070628, 'key':'RMGHS'},
-{'name': 'PARK NB','lat':42.35639457, 'lon':-71.0624242, 'key':'RPRKN'},
-{'name': 'PARK SB','lat':42.35639457, 'lon':-71.0624242, 'key':'RPRKS'},
-{'name': 'DOWNTOWN CROSSING NB','lat':42.355518, 'lon':-71.060225, 'key':'RDTCN'},
-{'name': 'DOWNTOWN CROSSING SB','lat':42.355518, 'lon':-71.060225, 'key':'RDTCS'},
-{'name': 'SOUTH STATION NB','lat':42.352271, 'lon':-71.055242, 'key':'RSOUN'},
-{'name': 'SOUTH STATION SB','lat':42.352271, 'lon':-71.055242, 'key':'RSOUS'},
-{'name': 'BROADWAY NB','lat':42.342622, 'lon':-71.056967, 'key':'RBRON'},
-{'name': 'BROADWAY SB','lat':42.342622, 'lon':-71.056967, 'key':'RBROS'},
-{'name': 'ANDREW NB','lat':42.330154, 'lon':-71.057655, 'key':'RANDN'},
-{'name': 'ANDREW SB','lat':42.330154, 'lon':-71.057655, 'key':'RANDS'},
-{'name': 'JFK NB','lat':42.320685, 'lon':-71.052391, 'key':'RJFKN'},
-{'name': 'JFK SB','lat':42.320685, 'lon':-71.052391, 'key':'RJFKS'},
-{'name': 'SAVIN HILL NB','lat':42.31129, 'lon':-71.053331, 'key':'RSAVN'},
-{'name': 'SAVIN HILL SB','lat':42.31129, 'lon':-71.053331, 'key':'RSAVS'},
-{'name': 'FIELDS CORNER NB','lat':42.300093, 'lon':-71.061667, 'key':'RFIEN'},
-{'name': 'FIELDS CORNER SB','lat':42.300093, 'lon':-71.061667, 'key':'RFIES'},
-{'name': 'SHAWMUT NB','lat':42.29312583,'lon':-71.06573796, 'key':'RSHAN'},
-{'name': 'SHAWMUT SB','lat':42.29312583, 'lon':-71.06573796, 'key':'RSHAS'},
-{'name': 'ASHMONT SB','lat':42.284652, 'lon':-71.064489, 'key':'RASHN'},
-{'name': 'NORTH QUINCY NB','lat': 42.275275, 'lon':-71.029583, 'key':'RNQUIN'},
-{'name': 'NORTH QUINCY SB','lat':42.275275, 'lon':-71.029583, 'key':'RNQUIS'},
-{'name': 'WOLLASTON NB','lat':42.2665139, 'lon':-71.0203369, 'key':'RWOLN'},
-{'name': 'WOLLASTON SB','lat':42.2665139, 'lon':-71.0203369, 'key':'RWOLS'},
-{'name': 'QUINCY CENTER NB','lat':42.233391, 'lon':-71.007153, 'key':'RQUCN'},
-{'name': 'QUINCY CENTER SB','lat':42.233391, 'lon':-71.007153, 'key':'RQUCS'},
-{'name': 'QUINCY ADAMS NB','lat':42.233391, 'lon':-71.007153, 'key':'RQUAN'},
-{'name': 'QUINCY ADAMS SB','lat':42.233391, 'lon':-71.007153, 'key':'RQUAS'},
-{'name': 'BRAINTREE SB','lat':42.2078543,'lon':-71.0011385, 'key':'RBRAS'}];
-
+{'name': 'ALEWIFE',			  'key': 'RALE', 'directions': ['N', null],'lat': 42.395428, 'lon': -71.142483},
+{'name': 'DAVIS' , 			  'key': 'RDAV', 'directions': ['N','S'], 'lat':42.39674,   'lon': -71.121815},
+{'name': 'PORTER',  		  'key': 'RPOR', 'directions': ['N','S'], 'lat':42.3884, 	  'lon':-71.119149},
+{'name': 'HARVARD', 		  'key': 'RHAR', 'directions': ['N','S'], 'lat':42.373362,  'lon':-71.118956},
+{'name': 'CENTRAL',           'key': 'RCEN', 'directions': ['N','S'], 'lat':42.365486,  'lon':-71.103802},
+{'name': 'KENDALL', 		  'key': 'RKEN', 'directions': ['N','S'], 'lat':42.36249079,'lon':-71.08617653},
+{'name': 'CHARLES MGH',		  'key': 'RMGH', 'directions': ['N','S'], 'lat':42.361166,  'lon':-71.070628},
+{'name': 'PARK',			  'key': 'RPRK', 'directions': ['N','S'], 'lat':42.35639457,'lon':-71.0624242},
+{'name': 'DOWNTOWN CROSSING', 'key': 'RDTC', 'directions': ['N','S'], 'lat':42.355518,  'lon':-71.060225},
+{'name': 'SOUTH STATION',     'key': 'RSOU', 'directions': ['N','S'], 'lat':42.352271,   'lon':-71.055242},
+{'name': 'BROADWAY',		  'key': 'RBRO', 'directions': ['N','S'], 'lat':42.342622, 'lon':-71.056967},
+{'name': 'ANDREW',		      'key': 'RAND', 'directions': ['N','S'], 'lat':42.330154, 'lon':-71.057655},
+{'name': 'JFK',				  'key': 'RJFK', 'directions': ['N','S'], 'lat':42.320685, 'lon':-71.052391}, //line splits at station 12
+{'name': 'SAVIN HILL',		  'key': 'RSAV', 'directions': ['N','S'], 'lat':42.31129, 'lon':-71.053331},
+{'name': 'FIELDS CORNER',	  'key': 'RFIE', 'directions': ['N','S'], 'lat':42.300093, 'lon':-71.061667},
+{'name': 'SHAWMUT', 		  'key': 'RSHA', 'directions': ['N','S'], 'lat':42.29312583,'lon':-71.06573796},
+{'name': 'ASHMONT',			  'key': 'RASH', 'directions': [null,'S'],'lat':42.284652, 'lon':-71.064489}, //end of line 1
+{'name': 'NORTH QUINCY',	  'key': 'RNQU', 'directions': ['N','S'],'lat': 42.275275, 'lon':-71.029583},
+{'name': 'WOLLASTON', 		  'key': 'RWOL', 'directions': ['N','S'], 'lat':42.2665139, 'lon':-71.0203369},
+{'name': 'QUINCY CENTER', 	  'key': 'RQUC', 'directions': ['N','S'], 'lat':42.233391, 'lon':-71.007153},
+{'name': 'QUINCY ADAMS', 	  'key': 'RQUA', 'directions': ['N','S'], 'lat':42.233391, 'lon':-71.007153},
+{'name': 'BRAINTREE', 		  'key': 'RBRA', 'directions': [null,'S'],'lat':42.2078543,'lon':-71.0011385}]; //end of line 2
 
 
 var myLat = 0;
@@ -101,7 +81,7 @@ function renderMap()
 
 function createStations()
 {
-    for (i = 0; i<42;i++){
+    for (i = 0; statios[i]!=null ;i++){
     	// Create a marker
     	var coords = new google.maps.LatLng(stations[i].lat, stations[i].lon);
     	var marker = new google.maps.Marker({
