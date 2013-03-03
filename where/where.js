7b5806898addca92beb2653156dd5d112a231b60
zoesobin <zoe.sobin@tufts.edu>
2013-03-03 15:59:19 -0500

    created stations

--- where/where.js
+++ where/where.js
@@ -88,6 +88,7 @@ function createStations()
     	var marker = new google.maps.Marker({
     		position: coords,
         	title: stations[i].name,
+        	icon: 'icon.png',
         	map: map
     	});
     	linecoords[i]= new google.maps.LatLng(stations[i].lat, stations[i].lon);
